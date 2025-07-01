0569d7ed48d2aaf3be5f5cd4501be0c4f95d945c
João Araujo de Souza <503392019@geaerospace.com>
2025-07-01 14:13:29 -0300

    first commit

---
+++ src/components/Clock.tsx
@@ -0,0 +1,3 @@
+export default function Clock() {
+  return <div>12:00</div>;
+}
---
+++ src/components/Stars.tsx
@@ -0,0 +1,5 @@
+import starsImage from '@/assets/stars.jpg';
+
+export default function Stars() {
+  return <img src={starsImage.src} alt="" />;
+}
